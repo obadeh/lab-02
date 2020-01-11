fbe9f559548c702487716ca920a33fb388cea969
obada <obadaquraan@yahoo.com>
2020-01-11 23:40:18 +0200

    practice

--- js/app.js
+++ js/app.js
@@ -11,26 +11,70 @@ function Horn(image_url,title,description,keyword,horns){
     Horn.all.push(this)
 }
 
+let container = $('#photo-template');
+
+Horn.prototype.render=function(){
+ // Create a new empty div tag
+ let hornOutput = $('<div></div>');
+ hornOutput.addClass(this.keyword);
+
+// clone (copy) the html from inside the photo-template
+let template = $('#photo-template').html();
+
+// Add the template to the output div
+hornOutput.html( template );
+
+// Put the data in
+hornOutput.find('h2').text( this.title );
+hornOutput.find('img').attr('src', this.image_url);
+hornOutput.find('p').text(this.description);
+
+$('main').append(hornOutput);
+
+
+
+};
+
+function renderOption(){
+    let options=[];
+    let option2=[];
+    Horn.all.forEach((val)=>{
+        options.push(val.keyword)
+        option2.push(val.title)
+    })
+    let unique = [...new Set(options)];
+    console.log(unique);
+    unique.forEach((val)=>{$('#Keyword').append(`<option>${val}</option>`)})
+    option2.forEach((val)=>{$('#Title').append(`<option>${val}</option>`)})
+
+}
+
+$('#Keyword').on('change',function(){
+    $('div').hide()
+    let selected= $(this).val()
+    $(`.${selected}`).show()
+    console.log('selected : ', selected);
+    
+})
+function renderTitle(){
+$('#Title').on('change',function(){
+    $('div').hide()
+    let filterd= $(this).val()
+
+    let fil= Horn.all.filter((value)=>{
+        return value.title===filterd;
+    })
+    $(`.${fil[0].keyword}`).show()
+console.log('fil : ', fil[0].keyword);
+    console.log(filterd)
+    // $(`.${selected2}`).show()
+    // console.log('selected : ', selected2);
+    
+})
+}
 $.get('/data/page-1.json').then(data=>{
     data.forEach(value => {
-     new Horn(value.image_url,value.title,value.description,value.keyword,value.horns)        
+    let hornss= new Horn(value.image_url,value.title,value.description,value.keyword,value.horns) 
+    hornss.render()       
     });
-})
-console.log('Horn.all : ', Horn.all);
-
-
-
-let container = $('#photo-template');
-$('#photo-template').append("<h1>hi</h1>");
-    $('#photo-template').append(`<img src="" alt="">`);
-
-$( document ).ready( function(){
-Horn.all.forEach((value)=>{console.log('value : ', value);})
-})
-
-for (let i = 0; i < Horn.all.length; i++) {
-    console.log(' Horn.all: ',Horn.all[i].title );
-    
-}
-let arr=[1,2,3,4,5];
-arr.forEach((value)=>{console.log('value : ', value);})
+}).then( () => renderOption() ).then(()=>renderTitle())
